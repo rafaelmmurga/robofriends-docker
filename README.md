7cdcef32b61e558f670461a2ab64e12e4f87ea1d
rafael <rafael.martinez@cevisur.com.mx>
2023-10-23 17:53:37 -0600

    readme

--- README.md
+++ README.md
@@ -1,24 +1,8 @@
-# With Docker
-
-This examples shows how to use Docker with Next.js based on the [deployment documentation](https://nextjs.org/docs/deployment#docker-image). Additionally, it contains instructions for deploying to Google Cloud Run. However, you can use any container-based deployment host.
-
-## How to use
-
-Execute [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app) with [npm](https://docs.npmjs.com/cli/init), [Yarn](https://yarnpkg.com/lang/en/docs/cli/create/), or [pnpm](https://pnpm.io) to bootstrap the example:
-
-```bash
-npx create-next-app --example with-docker nextjs-docker
-# or
-yarn create next-app --example with-docker nextjs-docker
-# or
-pnpm create next-app --example with-docker nextjs-docker
-```
-
 ## Using Docker
 
 1. [Install Docker](https://docs.docker.com/get-docker/) on your machine.
-1. Build your container: `docker build -t nextjs-docker .`.
-1. Run your container: `docker run -p 3000:3000 nextjs-docker`.
+1. Build your container: `docker build -t robofriends-docker .`.
+1. Run your container: `docker run -p 3000:3000 robofriends-docker`.
 
 You can view your images created with `docker images`.
 
--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,51 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react'
+import 'tachyons';
+
+import CardList from '../app/Presentation/components/CardList';
+import SearchBox from '../app/Presentation/components/SearchBox.js';
+import Scroll from '../app/Presentation/components/Scroll.js';
+
 
 export default function Home() {
+
+  const [robots, setRobots] = useState([])
+  const [cuadrobuscar, setCuadrobuscar] = useState("")
+
+  const robotFiltrado = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(cuadrobuscar.toLowerCase())
+  });
+
+  const enBuscarCambio = (evento) => {
+    setCuadrobuscar(evento.target.value)
+  }
+
+  useEffect(() => {
+    fetch('https://rafaelmmurga.github.io/datos/users.json')
+      .then(response => response.json())
+      .then(users => setRobots(users))
+  }, [])
+
   return (
-    <div className={styles.container}>
+    <div className='pa2 ma2'>
       <Head>
-        <title>Create Next App</title>
+        <title>RoboFriends</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main>
+        <div className='tc pa2 ma2'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={enBuscarCambio} />
+          <Scroll>
+            {!robots.length
+              ? <h1>Cargando</h1>
+              : <CardList robots={robotFiltrado} />
+            }
+          </Scroll>
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
