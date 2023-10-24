850d35f5db61727c9662e8e19e5f24e883419e8d
rafael <rafael.martinez@cevisur.com.mx>
2023-10-23 18:36:02 -0600

    assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'standalone',
-  basePath: process.env.BASE_PATH
+  basePath: process.env.BASE_PATH,
+  assetPrefix: process.env.NODE_ENV === "production" ? `${process.env.BASE_PATH}/` : undefined,
 }
