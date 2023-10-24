7e11fcf60de22141a0f321f5e5d77e4b0fe24c01
rafael <rafael.martinez@cevisur.com.mx>
2023-10-23 18:27:07 -0600

    path

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'standalone',
+  assetPrefix: process.env.SELF_PATH,
+  publicRuntimeConfig: {
+    SELF_PATH: process.env.SELF_PATH
+  }
 }
