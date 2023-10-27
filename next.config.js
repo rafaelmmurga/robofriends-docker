58ae0d032a3398d65c3d3689f37286ba2ad18fb9
rafael <rafael.martinez@cevisur.com.mx>
2023-10-26 18:19:29 -0600

    change config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  output: 'standalone',
-  basePath: process.env.BASE_PATH,
-  assetPrefix: `${process.env.BASE_PATH}/`,
+  output: 'standalone'
 }
